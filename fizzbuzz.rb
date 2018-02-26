967422493756f2d1e9d99cbc3a0a4dca2d16db4c
Jeremiah Lutes <jeremiahlutes@gmail.com>
2018-02-26 06:12:35 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,11 @@
 def fizzbuzz(num)
-  if num % == 0
+  if num % 3 == 0 && num % 5 == 0
+  "Fizzbuzz"
+  elsif num % 3 == 0
   "Fizz"
-  elsif 
+  elsif num & 5 == 0
+  "Buzz"
+  else 
+    nil
+  end
 end
